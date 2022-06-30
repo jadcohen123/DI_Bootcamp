9f12abe63b545ceb47890196a35ffd0e60d5c238
jadcohen123 <107697640+jadcohen123@users.noreply.github.com>
2022-06-30 16:26:19 +0300

    Add files via upload

---
+++ week2/day5-project/daily-challenge/script.js
@@ -0,0 +1,22 @@
+// Beer bottle song lyric
+
+function beerBottleSong(number, subtract) {
+    number = number - subtract;
+    return number;
+}
+let userNum = parseInt(prompt("Enter amount of bottles"));
+for (let i = 1; 0 < userNum; i++) {
+    console.log(`${userNum} bottles of beer on the wall`);
+    console.log(`${userNum} bottles of beer`);
+    if (userNum < i) {
+        i = userNum;
+    }
+    if (i == 1) {
+        console.log(`Take ${i} down, pass it around`);
+    } else {
+        console.log(`Take ${i} down, pass them around`);
+    }
+    userNum = beerBottleSong(userNum, i);
+    console.log(`${userNum} bottles of beer on the wall`);
+    console.log(" ");
+}
