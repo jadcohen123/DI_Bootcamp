b80d8c7449dc7d5f450e3c92c34944c3afa68118
jadcohen123 <107697640+jadcohen123@users.noreply.github.com>
2022-07-06 23:34:15 +0300

    Add files via upload

---
+++ week3/day3/exercise-xp/script.js
@@ -0,0 +1,7 @@
+//exercise I`
+let alrt = setTimeout(function() { alert("Hello World"); }, 2000);
+let para = document.getElementById("container");
+function getPara(){
+    para.createElement('p');
+}
+getPara();
