edb11234c759c86125eece760ea781f1fb25bd5a
jadcohen123 <107697640+jadcohen123@users.noreply.github.com>
2022-07-07 08:50:04 +0300

    Delete week3/day3/exercise-xp directory

--- week3/day3/exercise-xp/script.js
+++
@@ -1,7 +0,0 @@
-//exercise I`
-let alrt = setTimeout(function() { alert("Hello World"); }, 2000);
-let para = document.getElementById("container");
-function getPara(){
-    para.createElement('p');
-}
-getPara();
