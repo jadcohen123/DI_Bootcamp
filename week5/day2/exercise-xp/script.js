9562f5f8e26ea275f8b56ab00d185933f75512d9
jadcohen123 <107697640+jadcohen123@users.noreply.github.com>
2022-07-19 17:36:45 +0300

    Add files via upload

---
+++ week5/day2/exercise-xp/script.js
@@ -0,0 +1,26 @@
+// Exercise I
+let request = new XMLHttpRequest();
+function gif() {
+   
+    request.open("GET","https://api.giphy.com/v1/gifs/search?q=hilarious&rating=g&api_key=hpvZycW22qCjn5cRM1xtWB8NKq4dQ2My"); 
+    request.send();
+ 
+  }
+
+  request.onload = function () {
+    let obj1 = JSON.parse(request.response);
+    console.log(obj1); 
+  };
+  gif();
+
+  //Exercise II
+  let sungifs = new XMLHttpRequest();
+  function sun() {
+    sungifs.open("GET", "https://api.giphy.com/v1/gifs/search?q=sun&offset=2&limit=10&api_key=hpvZycW22qCjn5cRM1xtWB8NKq4dQ2My"); 
+    sungifs.send();
+  }
+  sungifs.onload = function () {
+    let obj2 = JSON.parse(sungifs.response);
+    console.log(obj2);
+  };
+  sun();
