f023f3c056e85a335fa6f9b457ad5bd545ec7628
jadcohen123 <107697640+jadcohen123@users.noreply.github.com>
2022-07-24 08:17:27 +0300

    Add files via upload

---
+++ week5/day5/exercise-xp/script.js
@@ -0,0 +1,33 @@
+let displayDiv = document.getElementById("displayDiv");
+
+const getCharacter = async () => {
+  displayDiv.innerHTML = `<i class="fas fa-spinner fa-spin fa-5x"></i>`
+  let randomNum = Math.floor((Math.random() * 88) + 1);
+
+  try {
+    let response = await fetch(`https://swapi.dev/api/people/${randomNum}/`);
+    let data = await response.json();
+    if (data.homeworld) {
+      data.homeworld = await getPlanet(data.homeworld);
+      display(data);
+    } else {
+      displayDiv.innerHTML = `Oh no, that person isnt available.`;
+    }
+  } catch {
+    displayDiv.innerHTML = `Oh no, that person isnt available.`;
+  }
+}
+
+const getPlanet = async (url) => {
+  let response = await fetch(url);
+  let data = await response.json();
+  return data.name;
+}
+
+const display = (data) => {
+  displayDiv.innerHTML = `<h1>${data.name}</h1>
+	<p>Height: ${data.height}</p>
+	<p>Gender: ${data.gender}</p>
+	<p>Birth Year: ${data.birth_year}</p>
+	<p>Home World: ${data.homeworld}</p>`;
+}
