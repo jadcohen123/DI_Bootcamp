b9595caddbe6ff4318902decd1dafeef6be90fcb
jadcohen123 <107697640+jadcohen123@users.noreply.github.com>
2022-08-08 02:32:07 +0300

    Add files via upload

---
+++ week8/day1/daily-challenge/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+// import the carousel
+import DemoCarousel from "./DemoCarousel";
+
+function App() {
+  return (
+    <div className="App">
+      <DemoCarousel />
+    </div>
+  );
+}
+
+export default App;
