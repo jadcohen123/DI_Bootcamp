9bbb40ac35168fa5b528e3285d35443c97fb020c
jadcohen123 <107697640+jadcohen123@users.noreply.github.com>
2022-08-16 18:01:47 +0300

    Add files via upload

---
+++ week9/day1/day2/dailychallenge/src/components/SearchBar.js
@@ -0,0 +1,28 @@
+
+import { connect } from "react-redux";
+import { filterArr } from "../actions";
+
+const SearchBar = ({ filterArr }) => {
+  return (
+    <div
+      style={{
+        position: "sticky",
+        top: "0",
+        borderBottom: "2px solid grey",
+        paddingBottom: "10px",
+      }}
+    >
+      <h3> Robot friend</h3>
+
+      <input type="text" onChange={filterArr} />
+    </div>
+  );
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    filterArr: (e) => dispatch(filterArr(e.target.value)),
+  };
+};
+
+export default connect(null, mapDispatchToProps)(SearchBar);
